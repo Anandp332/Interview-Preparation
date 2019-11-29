dab527b59a499bbb7d1987d15586648ca8ceb5ea
Anand Pandey <anandp332@gmail.com>
2019-11-29 20:08:10 +0530

    merge 2 sorted array

---
+++ ArrayAndString/mergeTwoSortedArray.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+using namespace std;
+void merge(int arr1[], int size1, int arr2[], int size2, int ans[]){
+    
+    /* Don't write main().
+     * Don't read input, it is passed as function argument.
+     * Save the merged array in ans[] array passed as argument.
+     * Don't return or print anything.
+     * Taking input and printing output is handled automatically.
+     */
+    int i,j,k;
+  for(i=0,j=0,k=0;k<size1+size2;k++)
+  {
+    if(i==(size1))
+    {
+      ans[k]=arr2[j];
+      j++;
+    }
+    else if(j==(size2))
+    {
+      ans[k]=arr1[i];
+      i++; 
+    }
+    else if(arr1[i]>=arr2[j])
+    {
+      ans[k]=arr2[j];
+      j++;
+    }
+    else if(arr1[i]<arr2[j])
+    {
+      ans[k]=arr1[i];
+      i++;
+    }
+  }
+    
+    
+}
+
+
+
+int main(){
+  int size1;  
+  cin >> size1;
+  int* arr1 = new int[size1];
+  for(int i = 0; i < size1; i++){
+    cin >> arr1[i];
+  } 
+  int size2;  
+  cin >> size2;
+  int* arr2 = new int[size2];
+  for(int i = 0; i < size2; i++){
+    cin >> arr2[i];
+  } 
+	
+  int* ans = new int[size1 + size2];
+	
+  merge(arr1, size1, arr2, size2, ans);
+
+  for(int i = 0; i < size1 + size2; i++){
+			cout << ans[i] <<  " ";
+		}
+  delete arr1;
+  delete arr2;
+  delete ans;
+}
+
+
